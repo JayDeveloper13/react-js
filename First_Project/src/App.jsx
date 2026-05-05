f2b9ba0b0b49ddd093ab4ad3fae1857cc1002e2b
Jay Jangid <developertest132000@gmail.com>
2026-05-05 15:22:11 +0530

    Reactjs learning

---
+++ First_Project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>This is arrow function</h1>
+      <h2>this is h2 tag</h2>
+    </>
+  )
+}
+
+export default App
---
+++ script.js
@@ -0,0 +1,9 @@
+// var i = document.createElement("h1");
+
+// i.innerHTML = "H1 tag via Js";
+
+// document.body.appendChild(i);
+import a from './app.js'
+
+ console.log(a);
+
